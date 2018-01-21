97994410fa350e4d3638e61e93dccffa099a8733
Fernando Rodríguez-Izquierdo Serrano <fernando@rodriguezizquierdo.com>
2018-01-21 11:34:29 +0100

    Se refactorizan variables y se ordena el calculo de unidades disponibles

--- volume/refactor/anexo_1.php
+++ volume/refactor/anexo_1.php
@@ -11,52 +11,41 @@ class Product
 	) {
 		if ($cache) {
 			// Obtenemos el stock bloqueado por pedidos en curso
-			$ordersQuantity = OrderLine::getDb()->cache(
+			$blockedOrders = OrderLine::getDb()->cache(
 				OrderLineCommonQuerys::getBlockedOrders($productId),
 				$cacheDuration
 			);
 
 			// Obtenemos el stock bloqueado
-			$blockedStockQuantity = BlockedStock::getDb()->cache(
+			$blockedStock = BlockedStock::getDb()->cache(
 				BlockedStockCommonQuerys::getBlockedStock($productId),
 				$cacheDuration
 			);
 		} else {
 			// Obtenemos el stock bloqueado por pedidos en curso
-			$ordersQuantity = OrderLineCommonQuerys::getBlockedOrders($productId);
+			$blockedOrders = OrderLineCommonQuerys::getBlockedOrders($productId);
 
 			// Obtenemos el stock bloqueado
-			$blockedStockQuantity = BlockedStockCommonQuerys::getBlockedStock($productId,true);
+			$blockedStock = BlockedStockCommonQuerys::getBlockedStock($productId,true);
 		}
 
+
 		// Calculamos las unidades disponibles
-		if (isset($ordersQuantity) || isset($blockedStockQuantity)) {
-			if ($quantityAvailable >= 0) {
-				$quantity = $quantityAvailable - @$ordersQuantity - @$blockedStockQuantity;
-				if (!empty($securityStockConfig)) {
-					$quantity = ShopChannel::applySecurityStockConfig(
-						$quantity,
-						@$securityStockConfig->mode,
-						@$securityStockConfig->quantity
-					);
-				}
-				return $quantity > 0 ? $quantity : 0;
-			} elseif ($quantityAvailable < 0) {
-				return $quantityAvailable;
-			}
-		} else {
-			if ($quantityAvailable >= 0) {
-				if (!empty($securityStockConfig)) {
-					$quantityAvailable = ShopChannel::applySecurityStockConfig(
-						$quantityAvailable,
-						@$securityStockConfig->mode,
-						@$securityStockConfig->quantity
-					);
-				}
-				$quantityAvailable = $quantityAvailable > 0 ? $quantityAvailable : 0;
-			}
-			return $quantityAvailable;
+		if (isset($blockedOrders) || isset($blockedStock))
+		{
+			$quantityAvailable = $quantityAvailable - $blockedOrders - $blockedStock;
 		}
-		return 0;
+
+		if ($quantityAvailable >= 0) {
+			if (!empty($securityStockConfig)) {
+				$quantityAvailable = ShopChannel::applySecurityStockConfig(
+					$quantityAvailable,
+					$securityStockConfig->mode,
+					$securityStockConfig->quantity
+				);
+			}
+		}
+
+		return $quantityAvailable;
 	}
 }
